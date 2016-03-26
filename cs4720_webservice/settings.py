bd24187ef318566a1cff2ead9c45fa227e68ef3c
zeizyy <yz9fy@virginia.edu>
2016-03-26 17:37:29 -0400

    heroku config

--- cs4720_webservice/settings.py
+++ cs4720_webservice/settings.py
@@ -75,12 +75,12 @@ WSGI_APPLICATION = 'cs4720_webservice.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.8/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=500)
