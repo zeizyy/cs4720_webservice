237baa35cb15d9d4321c90b5f4ba56f45d588c1f
zeizyy <yz9fy@virginia.edu>
2016-03-26 18:00:14 -0400

    heroku config

--- cs4720_webservice/urls.py
+++ cs4720_webservice/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^webservice/', include('webservice.urls'))
+    url(r'^webservice/', include('webservice.urls')),
+    url(r'^', 'webservice.views.error'),
+
 ]
