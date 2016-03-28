774067e766389c85d950434697612d1c5f64ec2c
Qian Xiong <qx2dk@virginia.edu>
2016-03-28 11:11:32 -0400

    update model

--- webservice/models.py
+++ webservice/models.py
@@ -1,32 +1,38 @@
 from django.db import models
 from django.contrib.auth import hashers
 import datetime
+
+
 # Create your models here.
 
 class User(models.Model):
-	username = models.CharField(max_length=24, unique=True)
-	password = models.CharField(max_length=96)
+    username = models.CharField(max_length=24, unique=True)
+    password = models.CharField(max_length=96)
 
-	def save(self, *args, **kwargs):
-		password = hashers.make_password(self.password)
-		self.password = password
-		super(User, self).save(*args, **kwargs)
+    def save(self, *args, **kwargs):
+        password = hashers.make_password(self.password)
+        self.password = password
+        super(User, self).save(*args, **kwargs)
 
-	def __unicode__(self):
-		return self.username
+    def __str__(self):
+        return self.username
+
 
 class Authenticator(models.Model):
-	authenticator = models.CharField(max_length=100, primary_key=True)
-	user_id = models.IntegerField()
-	date_created = models.DateTimeField(default=datetime.datetime.now)
+    authenticator = models.CharField(max_length=100, primary_key=True)
+    user_id = models.IntegerField()
+    date_created = models.DateTimeField(default=datetime.datetime.now)
 
-	def __unicode__(self):
-		return str(self.user_id)
+    def __str__(self):
+        return str(self.user_id)
+
 
 class Event(models.Model):
-	name = models.CharField(max_length=100)
-	user = models.ForeignKey(User, on_delete=models.CASCADE)
-	last_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=100)
+    detail = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    last_modified = models.DateTimeField(auto_now=True)
+    created = models.DateTimeField(auto_now_add=True)
 
-	def __unicode__(self):
-		return self.name
+    def __str__(self):
+        return self.name
